f36970074ede31ea4bc1803fe88c9ddcf5c605b0
ANDRES AROCHA <andresarocha0398@gmail.com>
2023-11-20 22:03:35 -0300

    Agregando

--- PreEntrega2/project-preentrega/src/components/ItemListContainer/ItemListContainer.jsx
+++ PreEntrega2/project-preentrega/src/components/ItemListContainer/ItemListContainer.jsx
@@ -7,27 +7,26 @@ export const ItemListContainer = ({ greeting }) => {
   const { category } = useParams();
 
   const [products, setProducts] = useState([]);
-  // Creamos un state que nos permite setear el estado de carga de los productos, inicializa en true
+  
   const [isLoading, setIsLoading] = useState(true);
 
 
-  // Con nuestro useEffect llamamos nuestros productos  
   useEffect(() => {
 
-    setIsLoading(true); // Cada ves que se cargue nuestro componente vamos a setear en true el isLoading para que nos aparezca la carga 
+    setIsLoading(true);
     getProducts()
       .then((resp) => {
-        // Si existe una category en el parámetro 
+        
         if(category) {
-          // Filtramos los productos de la respuesta por la categoría recibida por parámetro
+          
         const productsFilter = resp.filter(product => product.category === category);
-        // Almacenamos en nuestro state los productos filtrados
+        
         setProducts(productsFilter);
-        // Pasamos isLoading a false para que se rendericen los productos filtrados
+          
         setIsLoading(false);
         
         } else {
-          // En caso que no tengamos ninguna categoría en los parámetros directamente seteamos todos los productos en el state products
+          
           setProducts(resp);
         setIsLoading(false);
           
@@ -35,13 +34,13 @@ export const ItemListContainer = ({ greeting }) => {
  
       })
       .catch((error) => console.log(error));
-  }, [category]); // En el array de dependencia controlamos si se modifica el valor de la category para recargar el componente
+  }, [category]); 
 
 
   return (
     <>
       <div> {greeting} </div>
-      {/* si isLoading está en true va a mostrar Cargando productos... pero cuando se pase a false va a renderizar los productos */}
+      {}
       { isLoading ? <h2>Cargando productos ..</h2> : <ItemList products={products} /> }
     </>
   );
